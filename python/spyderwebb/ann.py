57c0a48829dc6d37ffd1398a361eb5b6619fc67a
David Nidever <dnidever@lsst.org>
2023-10-25 21:26:28 -0600

    updated monte.py and ann.py to use the new jwst ann

--- python/spyderwebb/ann.py
+++ python/spyderwebb/ann.py
@@ -17,10 +17,10 @@ class JWSTANNModel():
     
     def __init__(self,spobs=None,loggrelation=False,verbose=False):
         # Load the ANN models
-        emwarm = Emulator.read(utils.datadir()+'ann_28pars_warm.pkl')
-        emcool = Emulator.read(utils.datadir()+'ann_28pars_cool.pkl')
-        emwarm_mp = Emulator.read(utils.datadir()+'ann_28pars_warm_metalpoor.pkl')
-        self._models = [emcool,emwarm,emwarm_mp]
+        em1 = Emulator.read(utils.datadir()+'ann_29pars_3500-4200.pkl')
+        em2 = Emulator.read(utils.datadir()+'ann_29pars_4000-5000.pkl')
+        em3 = Emulator.read(utils.datadir()+'ann_29pars_4900-6000.pkl')
+        self._models = [em1,em2,em3]
         self.nmodels = len(self._models)
         self.labels = self._models[0].label_names
         self.nlabels = len(self.labels)
@@ -29,11 +29,12 @@ class JWSTANNModel():
             for j in range(self.nlabels):
                 self._ranges[i,j,:] = [np.min(self._models[i].training_labels[:,j]),
                                        np.max(self._models[i].training_labels[:,j])]
-        self._ranges[0,0,1] = 4100.0
-        self._ranges[1,0,0] = 4100.0        
+        self._ranges[0,0,1] = 4100.0  # use 3500-4200 model up to 4100
+        self._ranges[1,0,0] = 4100.0  # use 4000-5000 model from 4100 to 4950
+        self._ranges[1,0,1] = 4950.0        
+        self._ranges[2,0,0] = 4950.0  # use 4900-6000 model from 4950
         self.ranges = np.zeros((self.nlabels,2),float)
-        self.ranges[0,:] = [self._ranges[0,0,0],self._ranges[1,0,1]]
-        for i in np.arange(1,self.nlabels):
+        for i in range(self.nlabels):
             self.ranges[i,:] = [np.max(self._ranges[:,i,0]),np.min(self._ranges[:,i,1])]
         
         # Alpha element index
