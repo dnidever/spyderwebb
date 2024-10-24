d4fa6fc49e4ff773dc2a89afe8469977ff63ac76
David Nidever <dnidever@montana.edu>
2024-10-24 15:26:46 -0600

    maskgen.py updates

--- python/spyderwebb/maskgen.py
+++ python/spyderwebb/maskgen.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
+import json
 from gwcs.wcs import WCS
+import asdf
 import jwst
 from stdatamodels.jwst import datamodels
 from jwst.lib.wcs_utils import get_wavelengths
@@ -21,6 +23,8 @@ from jwst.assign_wcs import pointing,nirspec,assign_wcs
 from stpipe import crds_client
 from astropy.modeling import models
 
+from . import utils
+
 # Generate JWST NIRSpec MSA masks
 
 MSA2SLIT_COEF = np.array([[[ 4.07470547e+02,  9.52495217e+03, -4.15835866e+00],
@@ -32,6 +36,324 @@ MSA2SLIT_COEF = np.array([[[ 4.07470547e+02,  9.52495217e+03, -4.15835866e+00],
                           [[-4.28366954e+01,  9.52592872e+03, -2.22355660e+00],
                            [-3.46210821e+01,  1.14709013e+00,  4.91424362e+03]]])
 
+class NIRSpecTransform(object):
+
+    def __init__(self,grating='G140H',filt='F100LP',ra=11.54924651643522,dec=42.0959801450496,
+                 msafile=None):
+        instrument = 'nirspec'
+        input_model = datamodels.ImageModel()
+        input_model.meta.instrument.name = 'NIRSPEC'
+        input_model.meta.instrument.grating = grating
+        input_model.meta.instrument.filter = filt
+        input_model.meta.instrument.detector = 'NRS2'
+        input_model.meta.instrument.gwa_pxav = 179.1740788000057
+        input_model.meta.instrument.gwa_pyav = 66.73139400000171
+        input_model.meta.instrument.gwa_tilt = 36.06178279999935
+        input_model.meta.instrument.gwa_xp_v = 179.1855152
+        input_model.meta.instrument.gwa_xtilt = 0.3576895300000106
+        input_model.meta.instrument.gwa_yp_v = 66.731394
+        input_model.meta.instrument.gwa_ytilt = 0.1332439180000042
+        input_model.meta.instrument.msa_state = 'PRIMARYPARK_CONFIG'
+        if msafile is None:
+            msafile = utils.datadir()+'jw02609009001_04_msa.fits'
+        input_model.meta.instrument.msa_metadata_file = msafile
+        input_model.meta.instrument.msa_metadata_id = 313
+        input_model.meta.observation.date = '2024-10-22'
+        input_model.meta.observation.time = '22:31:26.597'
+        input_model.meta.exposure.type = 'NRS_MSASPEC'
+        input_model.meta.dither.position_number = 1
+        input_model.meta.wcsinfo.ra_ref = ra
+        input_model.meta.wcsinfo.dec_ref = dec
+        input_model.meta.wcsinfo.v2_ref = 378.563202
+        input_model.meta.wcsinfo.v3_ref = -428.402832
+        input_model.meta.wcsinfo.v3yangle = 138.5745697
+        input_model.meta.wcsinfo.roll_ref = 89.30712467134603
+        input_model.meta.wcsinfo.velosys = 18316.66
+        input_model.meta.wcsinfo.vparity = -1
+        self.input_model = input_model
+        self._ra = ra
+        self._dec = dec
+        
+        reference_file_types = ['distortion', 'filteroffset', 'specwcs', 'regions',
+                                'wavelengthrange', 'camera', 'collimator', 'disperser',
+                                'fore', 'fpa', 'msa', 'ote', 'ifupost',
+                                'ifufore', 'ifuslicer', 'msaoper']
+        reference_file_names = {}
+        for reftype in reference_file_types:
+            reffile = crds_client.get_reference_file(input_model.get_crds_parameters(), reftype, 'jwst')
+            reference_file_names[reftype] = reffile if reffile else ""
+
+        self.reference_file_types = reference_file_types
+        self.reference_file_names = reference_file_names
+        
+        slit_y_range = [-.55, .55]
+        self.slit_y_range = slit_y_range
+        
+        msa_pipeline = nirspec.slits_wcs(input_model,reference_file_names,slit_y_range)
+        self.msa_pipeline = msa_pipeline
+        msa_wcs = WCS(msa_pipeline)
+        self.msa_wcs = msa_wcs
+        #self._world2detector = msa_wcs.get_transform('world','detector')
+        
+        # msa_wcs.available_frames
+        # ['detector', 'sca', 'gwa', 'slit_frame', 'msa_frame', 'oteip', 'v2v3', 'v2v3vacorr', 'world']
+    
+        # load_wcs() creates a gWCS object and stores it in input_model.meta
+        result = assign_wcs.load_wcs(input_model, reference_file_names, slit_y_range)
+        # in load_wcs()
+        # from gwcs.wcs import WCS
+        # wcs = WCS(pipeline)
+        # output_model.meta.wcs = wcs
+    
+        imaging_pipeline = nirspec.imaging(input_model, reference_file_names)
+        self.imaging_pipeline = imaging_pipeline
+        imaging_wcs = WCS(imaging_pipeline)
+        self.imaging_wcs = imaging_wcs
+        
+        # imaging_wcs.available_frames
+        # ['detector', 'sca', 'gwa', 'msa', 'oteip', 'v2v3', 'v2v3vacorr', 'world']
+    
+        world2msa = imaging_wcs.get_transform('world','msa')
+        self._world2msa = world2msa
+
+        # Load shutter status information
+        with open(reference_file_names['msaoper']) as f:
+            shutterinfo = json.load(f)
+        dt = [('Q',int),('x',int),('y',int),('state',str,10),('TAstate',str,10),
+              ('Internalstate',str,10),('Vignetted',str,10)]
+        shutterstate = np.zeros(len(shutterinfo['msaoper']),dtype=np.dtype(dt))
+        shutterid = np.zeros(len(shutterinfo['msaoper']),(str,10))
+        for i,a in enumerate(shutterinfo['msaoper']):
+            shutterstate['Q'][i] = a['Q']
+            shutterstate['x'][i] = a['x']
+            shutterstate['y'][i] = a['y']
+            shutterstate['state'][i] = a['state']                    # "closed" or "open"
+            shutterstate['TAstate'][i] = a['TA state']               # "closed" or "open"
+            shutterstate['Internalstate'][i] = a['Internal state']   # "closed", "open" or "normal"
+            shutterstate['Vignetted'][i] = a['Vignetted']            # "no" or "yes"
+            shutterid[i] = str(a['Q'])+'-'+str(a['x'])+'-'+str(a['y'])
+        self._shutterstate = shutterstate
+        # shutter ID for quick lookup, q-c-r
+        self._shutterstate_id = shutterid
+
+        # Get MSA model information
+        self.msa = asdf.open(self.reference_file_names['msa'])
+        
+            
+    def world2msa(self,ra,dec):
+        """ Convert ra/dec to coordiantes in the MSA frame."""
+        return self._world2msa(ra,dec)
+
+    def world2slit(self,ra,dec):
+        """ Get slit quadrant/row/column information for in put ra/dec coordinates."""
+        # xmsa/ymsa have units of meters in the MSA plane/frame
+        xmsa,ymsa = self._world2msa(ra,dec)
+        quadrant,column,row,xslit,yslit = msa2slit(xmsa,ymsa)
+        return quadrant,column,row,xslit,yslit
+
+    def update_refcoords(self,ra,dec):
+        """ Change the reference coordinates."""
+        # quickly updates the input_model and all the wcs pipelines and objects
+        self._ra = ra
+        self.input_model.meta.wcsinfo.ra_ref = ra
+        self._dec = dec
+        self.input_model.meta.wcsinfo.dec_ref = dec
+        # imaging wcs pipeline
+        #    imaging_pipeline = [(det, dms2detector),
+        #                        (sca, det2gwa),
+        #                        (gwa, gwa2msa),
+        #                        (msa_frame, msa2oteip),
+        #                        (oteip, oteip2v23),
+        #                        (v2v3, va_corr),
+        #                        (v2v3vacorr, tel2sky),
+        #                        (world, None)]
+        # We only need to update tel2sky
+        tel2sky = pointing.v23tosky(self.input_model)
+        self.imaging_pipeline[-2] = (self.imaging_pipeline[-2][0],tel2sky)
+        # Remake imaging_wcs
+        self.imaging_wcs = WCS(self.imaging_pipeline)
+        # Remake world2msa
+        self._world2msa = self.imaging_wcs.get_transform('world','msa')
+        
+        # slits msa wcs pipeline
+        # msa_pipeline = [(det, dms2detector),
+        #                 (sca, det2gwa),
+        #                 (gwa, gwa2slit),
+        #                 (slit_frame, slit2msa),
+        #                 (msa_frame, msa2oteip),
+        #                 (oteip, oteip2v23),
+        #                 (v2v3, va_corr),
+        #                 (v2v3vacorr, tel2sky),
+        #                 (world, None)]
+        self.msa_pipeline[-2] = (self.msa_pipeline[-2][0],tel2sky)
+        # Remake msa_wcs
+        self.msa_wcs = WCS(self.msa_pipeline)
+        # Remake world2detector
+        self._world2detector = self.msa_wcs.get_transform('world','detector')
+        
+    @property
+    def ra(self):
+        return self._ra
+
+    @property
+    def dec(self):
+        return self._dec
+
+    def shutterstate(self,quadrant,column,row):
+        """ Return the state of the requested shutters shutters"""
+        squadrant = np.atleast_1d(quadrant).astype(str)
+        scolumn = np.atleast_1d(column).astype(str)
+        srow = np.atleast_1d(row).astype(str)
+        shutterid = np.char.array(squadrant)+'-'+np.char.array(scolumn)+'-'+np.char.array(srow)
+        _,ind1,ind2 = np.intersect1d(shutterid,self._shutterstate_id,return_indices=True)
+        # Initialize the output tables
+        dt = [('Q',int),('x',int),('y',int),('status',str,10),('state',str,10),
+              ('TAstate',str,10),('Internalstate',str,10),('Vignetted',str,10)]        
+        state = np.zeros(len(squadrant),dtype=np.dtype(dt))
+        state['Q'] = quadrant
+        state['x'] = column
+        state['y'] = row
+        state['status'] = 'okay'
+        if len(ind1)>0:
+            state['status'][ind1] = 'broken'
+            for c in self._shutterstate.dtype.names:
+                state[c][ind1] = self._shutterstate[c][ind2]
+        return state
+
+    def gwa_to_slit(self,quadrant,column,row):
+        """ Get gwa2slit transformation for a single slit."""
+        agreq = nirspec.angle_from_disperser(self.disperser, self.input_model)
+        collimator2gwa = nirspec.collimator_to_gwa(self.reference_file_names, self.disperser)
+        lgreq = nirspec.wavelength_from_disperser(self.disperser, self.input_model)
+        try:
+            velosys = self.input_model.meta.wcsinfo.velosys
+        except AttributeError:
+            pass
+        else:
+            if velosys is not None:
+                velocity_corr = nirspec.velocity_correction(self.input_model.meta.wcsinfo.velosys)
+                lgreq = lgreq | velocity_corr
+                log.info("Applied Barycentric velocity correction : {}".format(velocity_corr[1].amplitude.value))
+
+        # The wavelength units up to this point are
+        # meters as required by the pipeline but the desired output wavelength units is microns.
+        # So we are going to Scale the spectral units by 1e6 (meters -> microns)
+        is_lamp_exposure = self.input_model.meta.exposure.type in ['NRS_LAMP', 'NRS_AUTOWAVE', 'NRS_AUTOFLAT']
+        if self.input_model.meta.instrument.filter == 'OPAQUE' or is_lamp_exposure:
+            lgreq = lgreq | Scale(1e6)
+
+        msa_quadrant = self.msa['Q'+str(quadrant)]
+        msa_model = msa_quadrant.model
+        msa_data = msa_quadrant.data
+        # Slit('S1600A1', 3, 0, 0, 0, slit_y_range[0], slit_y_range[1], 5, 1)
+        #slit = models.Slit(slitlet_id, shutter_id, dither_position,
+        #                   xcen, ycen, ymin, ymax, quadrant, source_id,
+        #                   all_shutters, source_name, source_alias,
+        #                   stellarity, source_xpos, source_ypos,
+        #                   source_ra, source_dec)
+        #slit = Slit('S1600A1', s+1, 0, 0, 0, slit_y_range[0], slit_y_range[1], q, 1)
+        slit = Slit('S1600A1', s+1, 0, 0, 0, self.slit_y_range[0], self.slit_y_range[1], quadrant, 1)
+        # slit_id is a running number from 1 to 62415
+        slit_id = (row-1)*365 + column   # CHECK THIS!!
+        slit.shutter_id = slit_id
+        
+        #mask = mask_slit(slit.ymin, slit.ymax)
+        #slit_id = slit.shutter_id
+        # Shutter IDs are numbered starting from 1
+        # while FS are numbered starting from 0.
+        # "Quadrant 5 is for fixed slits.
+        #if quadrant != 5:
+        #    slit_id -= 1
+        slitdata = msa_data[slit_id-1]
+        slitdata_model = nirspec.get_slit_location_model(slitdata)
+        msa_transform = (slitdata_model | msa_model)
+        msa2gwa = (msa_transform | collimator2gwa)
+        gwa2msa = nirspec.gwa_to_ymsa(msa2gwa, slit=None, slit_y_range=self.slit_y_range)  # TODO: Use model sets here
+        #gwa2msa = nirspec.gwa_to_ymsa(msa2gwa, slit=slit, slit_y_range=(slit.ymin, slit.ymax))  # TODO: Use model sets here
+        bgwa2msa = Mapping((0, 1, 0, 1), n_inputs=3) | \
+            Const1D(0) * Identity(1) & Const1D(-1) * Identity(1) & Identity(2) | \
+            Identity(1) & gwa2msa & Identity(2) | \
+            Mapping((0, 1, 0, 1, 2, 3)) | Identity(2) & msa2gwa & Identity(2) | \
+            Mapping((0, 1, 2, 3, 5), n_inputs=7) | Identity(2) & lgreq | mask
+        #   Mapping((0, 1, 2, 5), n_inputs=7) | Identity(2) & lgreq | mask
+        # and modify lgreq to accept alpha_in, beta_in, alpha_out
+        # msa to before_gwa
+        msa2bgwa = msa2gwa & Identity(1) | Mapping((3, 0, 1, 2)) | agreq
+        bgwa2msa.inverse = msa2bgwa
+        #slit_models.append(bgwa2msa)
+        #slits.append(slit)
+        return Gwa2Slit([slit], [bgwa2msa])
+
+    self slit_to_msa(self,quadrant,column,row):
+    def slit_to_msa(open_slits, msafile):
+        """
+        The transform from ``slit_frame`` to ``msa_frame``.
+        
+        Parameters
+        ----------
+        open_slits : list
+           A list of slit IDs for all open shutters/slitlets.
+        msafile : str
+          The name of the msa reference file.
+
+        Returns
+        -------
+        model : `~stdatamodels.jwst.transforms.Slit2Msa` model.
+           Transform from ``slit_frame`` to ``msa_frame``.
+        """
+        msa_quadrant = self.msa['Q'+str(quadrant)]
+        msa_data = msa_quadrant.data
+        msa_model = msa_quadrant.model
+        slit = Slit('S1600A1', s+1, 0, 0, 0, self.slit_y_range[0], self.slit_y_range[1], quadrant, 1)
+        # slit_id is a running number from 1 to 62415
+        slit_id = (row-1)*365 + column   # CHECK THIS!!
+        slit.shutter_id = slit_id
+        # Shutters are numbered starting from 1.
+        # Fixed slits (Quadrant 5) are mapped starting from 0.
+        #if quadrant != 5:
+        #    slit_id = slit_id - 1
+        slitdata = msa_data[slit_id-1]
+        slitdata_model = nirspec.get_slit_location_model(slitdata)
+        msa_transform = slitdata_model | msa_model
+        #models.append(msa_transform)
+        #slits.append(slit)
+        return Slit2Msa([slit], [msa_transform])
+    
+    def world2detector(self,ra,dec):
+        quadrant,column,row,xslit,yslit = self.world2slit(ra,dec)
+        # the msa_pipeline/wcs is configured for particular slits
+        # need to generate new wcs for each slit
+        n = np.atleast_1d(quadrant).size
+        for i in range(n):
+            msa_pipeline = self.msa_pipeline.copy()
+            # msa_pipeline = [(det, dms2detector),
+            #                 (sca, det2gwa),
+            #                 (gwa, gwa2slit),
+            #                 (slit_frame, slit2msa),
+            #                 (msa_frame, msa2oteip),
+            #                 (oteip, oteip2v23),
+            #                 (v2v3, va_corr),
+            #                 (v2v3vacorr, tel2sky),
+            #                 (world, None)]
+            # gwa2slit and slit2msa need to be modified
+            gwa2slit = msa_pipeline[2][1]
+            slit2msa = msa_pipeline[3][1]            
+
+            # GWA to SLIT
+            gwa2slit = self.gwa_to_slit(quadrant,column,row)
+            #gwa2slit = nirspec.gwa_to_slit(open_slits_id, self.input_model, disperser, self.reference_files_names)
+            gwa2slit.name = "gwa2slit"
+
+            # SLIT to MSA transform
+            slit2msa = self.slit_to_msa(quadrant,column,row)
+            #slit2msa = nirspec.slit_to_msa(open_slits_id, self.reference_files['msa'])
+            slit2msa.name = "slit2msa"
+            
+            
+    def traces(self,ra,dec):
+        """ Determine the traces on the detector."""
+        return self.msa_wcs(ra,dec)
+    
 def slitpositions(msa):
     """
     Get MSA positions for all of the slits/shutters
@@ -77,6 +399,7 @@ def msa2slit(xmsa,ymsa):
     """
     Convert xmsa/ymsa to slit quadrant/row/column
     """
+    # xmsa/ymsa have units of meters in the MSA plane/frame
     # [4,2,3],  [quadrant, row/column, coefficients]
     quadrant = (xmsa > 0)*2 + (ymsa > 0)*1 + 1
     ccoef = MSA2SLIT_COEF[quadrant-1,0,:]
@@ -86,11 +409,16 @@ def msa2slit(xmsa,ymsa):
     # convert to integer row/column and decimal xslit/yslit
     column = np.round(cc).astype(int)
     row = np.round(rr).astype(int)
+    # xslit/yslit values go from 0.0 to 1.0, 0.5 is the center of the shutter/slit
     xslit = cc-column + 0.5
     yslit = rr-row + 0.5
-    # xslit/yslit values go from 0.0 to 1.0, 0.5 is the center of the shutter/slit
-    return quadrant,column,row,xslit,yslit
+    # Note that the observation msa fits file has row and column reversed
     
+    return quadrant,column,row,xslit,yslit
+
+
+
+
 def maskgen(ra,dec):
     """
     Input coordinates and figure out where they will fall on the MSA shutter array
@@ -185,9 +513,9 @@ def maskgen(ra,dec):
     #  array([ 3.60360417e-05, -8.95968281e-04]))
     # what units are these, how do I convert it to a quadrant and row/column?
 
-    from astropy.io import ascii,fits
-    tab = ascii.read('M31-b19-rgb-fields1278.cat')
-    x,y = world2msa(tab['ra'],tab['dec'])
+    #from astropy.io import ascii,fits
+    #tab = ascii.read('M31-b19-rgb-fields1278.cat')
+    #x,y = world2msa(tab['ra'],tab['dec'])
 
 
     # slits msa wcs pipeline
